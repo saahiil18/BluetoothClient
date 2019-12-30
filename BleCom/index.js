c4470d990d0cbf59bdaf0eb1105a43dd71887619
Sahil Shaikh <sahil.shaikh@vendekin.com>
2019-12-30 16:15:30 +0530

    Bluetooth Added

--- BleCom/index.js
+++ BleCom/index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from './App'
 
-AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent('BleCom', () => App);
